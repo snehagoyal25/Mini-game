b2d3c9cea58faf2dbf50c4481a378e06949494b0
sneha <goyalsneha995@gmail.com>
2024-10-16 22:01:40 +0530

    X0

---
+++ fun.js
@@ -0,0 +1,23 @@
+// Accessing the boxes 
+let boxes=document.querySelectorAll(".box")
+// Accessing the reset button
+let resetButton=document.querySelector("#btn")
+
+let turnO = true;
+
+// winning patterns 
+const winPatterns =[
+[0,1,2],
+[0,3,6],
+[0,4,8],
+[1,4,7],
+[2,5,8],
+[2,4,6],
+[3,4,5],
+[6,7,8],
+];
+
+// Adding event listener to boxes so as a event can occur.
+boxes.forEach((box)=>{
+    box.addEventListener
+})
